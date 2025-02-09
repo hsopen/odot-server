2c4d2767d3ed4a2395fd63a68da38f4b922da580
hsopen <hsopen@outlook.com>
2025-02-09 23:00:37 +0800

    feat: 获取redis所需的环境变量

--- src/utils/getEnv.ts
+++ src/utils/getEnv.ts
@@ -10,4 +10,12 @@ export default {
   SMTP_HOST: env.get('SMTP_HOST').required().asString(),
   SMTP_PORT: env.get('SMTP_PORT').required().asPortNumber(),
   SMTP_SECURE: env.get('SMTP_SECURE').required().asBoolStrict(),
+
+  REDIS_HOST: env.get('REDIS_HOST').required().asString(),
+  REDIS_PORT: env.get('REDIS_PORT').required().asPortNumber(),
+  REDIS_USERNAME: env.get('REDIS_USERNAME').required().asString(),
+  REDIS_PASSWORD: env.get('REDIS_PASSWORD').required().asString(),
+  REDID_TLS: env.get('REDID_TLS').required().asJsonObject(),
+
+  EMAIL_EXPIRATION_TIME: env.get('EMAIL_EXPIRATION_TIME').required().asInt(),
 }
